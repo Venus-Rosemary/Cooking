a2db04044f26e73dd94673dce72b31eeed6036e7
DESKTOP-HDMLFNN\星品罡科技 <1608804518@qq.com>
2025-05-28 13:46:15 +0800

    优化柜台高亮、人物移动、餐盘UI lookat摄像机

---
+++ Assets/Scripts/UI/MenusAnimatorUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenusAnimatorUI : MonoBehaviour
+{
+    [SerializeField] private Transform oriPoint;
+    [SerializeField] private Transform tarPoint;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
